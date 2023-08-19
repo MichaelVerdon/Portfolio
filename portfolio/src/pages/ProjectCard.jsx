e1e1d96cae092dfdf8178cdb092cadfc5ff6f71c
MichaelVerdon <mwverdon2000@gmail.com>
2023-08-19 20:00:15 +0100

    uhhhh

--- portfolio/src/pages/ProjectCard.jsx
+++ portfolio/src/pages/ProjectCard.jsx
@@ -3,9 +3,14 @@ import React from 'react';
 const ProjectCard = ({ Project }) => {
     return(
         <>
-        <div className='Card'>
-            <img src={Project.src !== "N/A" ? Project.src : 'https://via.placeholder.com/400'} alt="project"/>
-        </div>
+            <div className='card'>
+                <img src={
+                    Project.src !== "N/A" 
+                    ? Project.src 
+                    : 'https://via.placeholder.com/400'
+                     } alt="project"/>
+                <div className='projectTitle'>{Project.name}</div>  
+            </div>      
         </>
     )
 }
--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,15 +1,20 @@
 import ProjectCard from './ProjectCard';
-import onyo from './images/onyo.jpg'
 
 export default function Projects(){
 
     const Project = {
         "name":"Test Project",
-        "src":{onyo},
+        "src":"https://www.zbrushcentral.com/uploads/default/original/3X/3/e/3e60ab859db110c6a2924f70633963fb966f0174.jpeg",
         "link":"http://www.google.com",
-
     }
     return(
-        <ProjectCard Project={Project}></ProjectCard>
+        <div className='projectsContainer'>
+            <ProjectCard Project={Project}></ProjectCard>
+            <ProjectCard Project={Project}></ProjectCard>
+            <ProjectCard Project={Project}></ProjectCard>
+            
+          
+        </div>
+        
     )
 }
