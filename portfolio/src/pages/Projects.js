37cc231febee71ffe83c2c957009a2c56e2d0cb9
Craig <sandedshoes@gmail.com>
2023-08-20 15:18:58 +0100

    I am Michael

--- portfolio/src/pages/Projects.js
+++ portfolio/src/pages/Projects.js
@@ -1,23 +1,20 @@
+import React from 'react';
 import ProjectCard from './ProjectCard';
 
-export default function Projects(){
-
+export default function Projects() {
     const Project = {
-        "name":"Test Project",
-        "src":"https://www.zbrushcentral.com/uploads/default/original/3X/3/e/3e60ab859db110c6a2924f70633963fb966f0174.jpeg",
-        "link":"http://www.google.com",
-    }
-    return(
+        "name": "Test Project",
+        "src": "https://www.zbrushcentral.com/uploads/default/original/3X/3/e/3e60ab859db110c6a2924f70633963fb966f0174.jpeg",
+        "link": "http://www.google.com",
+    };
+
+    const projectsArray = new Array(8).fill(Project); // Create an array of Project objects
+
+    return (
         <div className='projectsContainer'>
-            <ProjectCard Project={Project}></ProjectCard>
-            <ProjectCard Project={Project}></ProjectCard>
-            <ProjectCard Project={Project}></ProjectCard>
-            <ProjectCard Project={Project}></ProjectCard>
-            <ProjectCard Project={Project}></ProjectCard>
-            <ProjectCard Project={Project}></ProjectCard>
-            <ProjectCard Project={Project}></ProjectCard>
-            <ProjectCard Project={Project}></ProjectCard>
+            {projectsArray.map((project, index) => (
+                <ProjectCard key={index} Project={project} />
+            ))}
         </div>
-        
-    )
+    );
 }
