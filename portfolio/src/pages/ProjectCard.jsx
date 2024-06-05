6dc691101c2b80f5f79d5b0cbabddedbc86b7999
UP959856 <mwverdon2000@gmail.com>
2024-06-05 01:59:33 +0100

    Added modals

--- portfolio/src/pages/ProjectCard.jsx
+++ portfolio/src/pages/ProjectCard.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
+import { useState } from 'react';
 import { Col } from 'react-bootstrap';
+import Modal from 'react-modal';
+import gitHubLogo from "./images/github.svg"
 
 const ProjectCard = ({ Project }) => {
 
@@ -7,10 +10,14 @@ const ProjectCard = ({ Project }) => {
         window.open(Project.link)
     }
 
+    const [modalOpen, toggleModal] = useState(false);
+    const handleOpen = () => toggleModal(true);
+    const handleClose = () => toggleModal(false);
+
     return(
         <>
             <Col className='cardColumn'>
-                <div className='card' onClick={redirectLink}>
+                <div className='card' onClick={handleOpen}>
                     <img src={
                         Project.src !== "N/A" 
                         ? Project.src
@@ -19,11 +26,38 @@ const ProjectCard = ({ Project }) => {
                     <div className='projectTitle'>
                         <h3>{Project.name}</h3>
                     </div>
-                    <div className="projectDesc">
-                        <p>{Project.desc}</p>
+                    <div className="projectShow">
+                        <p>Click for more info</p>
                     </div>
                 </div> 
-            </Col>     
+            </Col>
+            <Modal isOpen={modalOpen} onRequestClose={handleClose} className="projectModal">
+             <div className='projectModalContents'>
+                <div className='modalImage'>
+                    <img src={
+                            Project.src !== "N/A" 
+                            ? Project.src
+                            : 'https://via.placeholder.com/400'
+                            } alt="project"/>
+                    </div>
+                <div className='projectTextContainer'>
+                    <div>
+                        <h3>{Project.name}</h3>
+                    </div>
+                    <div className="projectDesc">
+                        <p><strong>Description:</strong> {Project.desc}</p>
+                    </div>
+                    <div className='projectTechContainer'>
+                        <p><strong>Tech:</strong> {Project.tech}</p>
+                    </div>
+                    <div className="githubCont">
+                        <img src={gitHubLogo} alt="github" id="projectGithub" onClick={redirectLink}/>
+                        <p>Github</p>
+                    </div>     
+                </div>
+                
+             </div>
+            </Modal>  
         </>
     )
 }
