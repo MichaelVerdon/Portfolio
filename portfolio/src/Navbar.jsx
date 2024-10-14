fc7484eb6e2504dfbbba94d6306cd9118a5b7a80
MichaelVerdon <mwverdon2000@gmail.com>
2024-10-14 20:32:16 +0100

    New navbar with hamburger menu

--- portfolio/src/Navbar.jsx
+++ portfolio/src/Navbar.jsx
@@ -1,30 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const Navbar = ({ Logo }) => {
+    const [isOpen, setIsOpen] = useState(false);
 
-    return(
-        <nav className='nav'>
-            <div className='logoContainer'>
-                <a href='/'>
-                    <img src={Logo}/>
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <nav className="nav">
+            <div className="logoContainer">
+                <a href="/">
+                    <img src={Logo} alt="Logo" />
                 </a>
             </div>
-            <div className='navContentsContainer'>
-                <ul className='navList'>    
+            <div className={`navContentsContainer ${isOpen ? 'open' : ''}`}>
+                <ul className={`navList ${isOpen ? 'active' : ''}`}>
                     <li>
-                        <a href='/projects' className="header">Projects</a>
+                        <a href="/projects" className="header">Projects</a>
                     </li>
                     <li>
-                        <a href='/skills' className="header">Skills</a>
+                        <a href="/skills" className="header">Skills</a>
                     </li>
                     <li>
-                        <a href='/contact' className="header">Contact</a>
+                        <a href="/contact" className="header">Contact</a>
                     </li>
                 </ul>
             </div>
+            <div className="hamburger" onClick={toggleMenu}>
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+            </div>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
